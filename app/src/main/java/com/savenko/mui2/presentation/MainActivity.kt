6783d8c7064b11dd759d908d43a5e91f28a1f4ee
IvanSavenko <savenkoivan931@gmail.com>
2025-03-23 21:54:48 +0100

    change gradlew config, MainActivity.kt

--- app/src/main/java/com/savenko/mui2/presentation/MainActivity.kt
+++ app/src/main/java/com/savenko/mui2/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,37 +12,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mui1.data.CalendarCalculatorViewModel
+import com.example.mui1.presentation.CalendarSelectorScreen
 import com.savenko.mui2.presentation.theme.MUI2Theme
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val  viewModel : CalendarCalculatorViewModel by viewModels { CalendarCalculatorViewModel.Factory }
         enableEdgeToEdge()
         setContent {
             MUI2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CalendarSelectorScreen(paddingValues = innerPadding, viewModel = viewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MUI2Theme {
-        Greeting("Android")
     }
 }
